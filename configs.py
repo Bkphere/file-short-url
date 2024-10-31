7997ac71b1f49406c45b1259ce2fdd9bcd37e995
Bkphere <163238356+Bkphere@users.noreply.github.com>
2024-10-30 23:05:28 -0700

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,61 @@
+# (c) @PredatorHackerzZ || @TeleRoidGroup
+
+import os
+
+class Config(object):
+	API_ID = int(os.environ.get("API_ID", "28745135"))
+	API_HASH = os.environ.get("123e77214315877fe406e25f35afd23d")
+	BOT_TOKEN = os.environ.get("7358694327:AAFkgFORP5deJ3rU1Gu3P-XfLS6euI89fEM")
+	BOT_USERNAME = os.environ.get("Txtextractor1211bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002312870921"))
+	SHORTLINK_URL = os.environ.get('SHORTLINK_URL' "seturl.in")
+	SHORTLINK_API = os.environ.get('SHORTLINK_API' "40a2f999a5ab9adff0484f1ef1595c1684ab73c2")
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6750546542"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://fiona171593:tbGMvepmKQ8YNfJy@cluster0.5ccbrkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	OTHER_USERS_CAN_SAVE_FILE = [int(id) for id in os.environ.get("OTHER_USERS_CAN_SAVE_FILE", "").split(",") if id.strip()]
+	ABOUT_BOT_TEXT = f"""
+This is a Permanent FileStore Bot. 
+Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+
+╭────[ **🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅**]────⍟
+│
+├🔸 **My Name:** [FileStore Bot](https://t.me/{BOT_USERNAME})
+│
+├🔸 **Language:** [Python 3](https://www.python.org)
+│
+├🔹 **Library:** [Pyrogram](https://docs.pyrogram.org)
+│
+├🔹 **Hosted On:** [Heroku](https://heroku.com)
+│
+├🔸 **Developer:** [Predator HackerzZ](https://t.me/OwnYourBotz) 
+│
+├🔹 **Bot Support:** [Support Group](https://t.me/TeleRoid14)
+│
+├🔸 **Bot Updates:** [Bots Channel](https://t.me/TeleRoidGroup)
+│
+╰──────[ 😎 ]───────────⍟
+"""
+	ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@PredatorHackerzZ](https://github.com/PredatorHackerzZ)
+ 
+ I am Super noob Please Support My Hard Work.
+
+[Donate Me](https://t.me/DonateXrobot) or ```teleroidgroup@axl```
+"""
+	HOME_TEXT = """
+Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+
+How to Use Bot & it's Benefits??
+
+📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
+
+⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+
+❌ **PORNOGRAPHY CONTENTS** are strictly prohibited & get Permanent Ban.
+"""
